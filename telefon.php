b2b4d2a6eb29bc4df5dcc4a574fc6c5fbc2fa916
szkudlarzoskar <90000816+szkudlarzoskar@users.noreply.github.com>
2022-04-27 14:49:44 +0200

    Add files via upload

---
+++ telefon.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+input{
+text-align:center;
+background-color: grey;
+font-size: 20px;
+color: white;
+border-radius: 100%;
+width: 85px;
+-webkit-transition-duration: 0.4s;
+transition-duration: 0.4s;
+
+}
+div{
+text-align: center;
+display: inline-block;
+}
+input:hover{
+    background-color: white;
+    color: black;
+}
+</style>
+<div>
+<form>
+<input type="submit" name="1" value="  1  
+   ">
+<input type="submit" name="2" value="2
+ABC">
+<input type="submit" name="3" value="3
+DEF"><br>
+<input type="submit" name="4" value="4
+GHI">
+<input type="submit" name="5" value="5
+JKL">
+<input type="submit" name="6" value="6
+MNO"><br>
+<input type="submit" name="7" value="7
+PQRS">
+<input type="submit" name="8" value="8
+TUV">
+<input type="submit" name="9" value="9
+WXYZ"><br>
+<input type="submit" name="0" value="  0  
+   ">
+</form>
+</div>
+<?php
+
+
+
+///$food = 'cake';
+
+//$return_value = match ($) {
+ //   '2' => 'A',
+  //  '22' => 'B',
+    //'222' => 'C',
+ //   '3' => 'D',
+ //   '33' => 'E',
+  //  '333' => 'F',
+  //  '4' => 'G',
+  //  '44' => 'H',
+  //  '444' => 'I',
+  //  '5' => 'J',
+  //  '55' => 'K',
+  //  '555' => 'L',
+  //  '6' => 'M',
+  //  '66' => 'N',
+   // '666' => 'O',
+   // '7' => 'P',
+   // '77' => 'Q',
+   // '777' => 'R',
+   // '7777' => 'S',
+    //'8' => 'T',
+   // '88' => 'U',
+ //   '888' => 'V',
+ //   '9' => 'W',
+ //   '99' => 'X',
+  //  '999' => 'Y',
+   // '9999' => 'Z',
+//};
+
+//var_dump($return_value);
+
+
+
+?>
+</body>
+</html>
